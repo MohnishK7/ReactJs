044a191dd49aa9d312ae8223fb90739e713f1dc7
Mohnish Kumar <mohnishmadhav@gmail.com>
2024-03-19 02:01:07 +0530

    newdeployed

--- card-comp/src/App.jsx
+++ card-comp/src/App.jsx
@@ -1,11 +1,36 @@
 import Card from "./Card1";
+// below we dont use any props
+//props = read-only properties that are shared between components
+// a parent component can send data to a child component
+//<component key=value/>
+
+//propTypws = a mechanism that ensures that the passed value
+// is of correct datatype.
+// age : propType.number
+
+
+//and we have create the reusable component
+
+
 
 function App() {
   return (
     <>
-      <Card />
-      <Card />
-      <Card />
+      <Card
+        name="Albert"
+        title="I am a theoretical physicist"
+        imageSrc="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTCvRFkGvTCnWKTp010CB419AFy5SVRVQt9KfYtEcSC3Q&s"
+      />
+      <Card
+        name="Newton"
+        title="I create Calculs"
+        imageSrc="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f1/Unknown_man%2C_formerly_known_as_Sir_Isaac_Newton_from_NPG.jpg/200px-Unknown_man%2C_formerly_known_as_Sir_Isaac_Newton_from_NPG.jpg"
+      />
+      <Card
+        name="Max Plank"
+        title="Planck's quantum"
+        imageSrc="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/Max_Planck_%281858-1947%29.jpg/200px-Max_Planck_%281858-1947%29.jpg"
+      />
     </>
   );
 }
--- card-comp/src/Card1.jsx
+++ card-comp/src/Card1.jsx
@@ -1,15 +1,40 @@
-function Card() {
+import PropTypes from "prop-types"; // Import PropTypes
+
+function Card(props) {
   return (
     <div className="cards">
       <img
         className="card-img"
-        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTCvRFkGvTCnWKTp010CB419AFy5SVRVQt9KfYtEcSC3Q&s"
+        src={props.imageSrc} // Use props.imageSrc as the image source
         alt="profilepic"
       ></img>
-      <h2 className="card-title">Albert Einstein</h2>
-      <p className="card-text">Hi i am a theoretical physicist</p>
+      <h2 className="card-title">{props.name}</h2>
+      <p className="card-text">{props.title}</p>
     </div>
   );
 }
 
+// PropTypes validation
+Card.propTypes = {
+  name: PropTypes.string.isRequired, // name prop is required and must be a string
+  title: PropTypes.string.isRequired, // title prop is required and must be a string
+  imageSrc: PropTypes.string.isRequired, 
+};
+
 export default Card;
+
+// function Card() {
+//   return (
+//     <div className="cards">
+//       <img
+//         className="card-img"
+//         src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTCvRFkGvTCnWKTp010CB419AFy5SVRVQt9KfYtEcSC3Q&s"
+//         alt="profilepic"
+//       ></img>
+//       <h2 className="card-title">Albert Einstein</h2>
+//       <p className="card-text">Hi i am a theoretical physicist</p>
+//     </div>
+//   );
+// }
+
+// export default Card;
