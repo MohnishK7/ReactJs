68f9f24681efb0aa4fdfa1e1a6727830416e2e91
Mohnish Kumar <mohnishmadhav@gmail.com>
2024-03-18 19:52:43 +0530

    first commit

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,17 @@
+import Header from "./Header";
+import Footer from "./Footer";
+import Food from "./Food";
+
+//fragement are the empty set just to wrap thing in return statement
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Food />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
