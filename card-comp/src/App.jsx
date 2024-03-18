d29d0d815eb45a321765828a481035b3bbdcbfff
Mohnish Kumar <mohnishmadhav@gmail.com>
2024-03-19 01:07:47 +0530

    Deployed card

---
+++ card-comp/src/App.jsx
@@ -0,0 +1,13 @@
+import Card from "./Card1";
+
+function App() {
+  return (
+    <>
+      <Card />
+      <Card />
+      <Card />
+    </>
+  );
+}
+
+export default App;
---
+++ card-comp/src/Card1.jsx
@@ -0,0 +1,15 @@
+function Card() {
+  return (
+    <div className="cards">
+      <img
+        className="card-img"
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTCvRFkGvTCnWKTp010CB419AFy5SVRVQt9KfYtEcSC3Q&s"
+        alt="profilepic"
+      ></img>
+      <h2 className="card-title">Albert Einstein</h2>
+      <p className="card-text">Hi i am a theoretical physicist</p>
+    </div>
+  );
+}
+
+export default Card;
